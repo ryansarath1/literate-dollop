3ba1d82088177463f6812163dea90121e879c771
Ryan945 <sarathryan@yahoo.com>
2024-03-18 09:43:14 -0700

    boiler plated my functions

--- index.js
+++ index.js
@@ -18,5 +18,82 @@ function mainmenu(){
     }).render()
     console.log(logotext)
 
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "openingMessage",
+        message: "What would you like to do?",
+        choices: [
+          "viewAllEmployees",
+          "viewAllDepartments",
+          "viewAllRoles",
+          "addADepartment",
+          "addARole",
+          "addAEmployee",
+          "updateEmployee",          
+          "quit",
+        ],
+      },
+    ]).then((userchoice)=>{
+        let choice=userchoice.openingMessage
+        console.log(choice)
+        switch (choice) {
+            case "viewAllEmployees":
+              viewAllEmployees();
+              break;
+            case "viewAllDepartments":
+              viewAllDepartments();
+              break;
+            case "viewAllRoles":
+              viewAllRoles();
+              break;
+            case "addADepartment":
+              addADepartment();
+              break;
+            case "addARole":
+              addARole();
+              break;
+            case "addAEmployee":
+              addAEmployee();
+              break;
+            case "updateEmployee":
+              updateEmployee();
+              break;       
+            case "quit":
+              quit();
+              break;
+            default:
+              console.log("somethings wrong with you");
+              break;
+          }
+    })
+
+}
+function viewAllDepartments(){
+
+}
+function viewAllRoles(){
+
+}
+function viewAllEmployees(){
+
+}
+function addADepartment(){
+
+}
+function addARole(){
+
+}
+function addAEmployee(){
+
+}
+function updateEmployee(){
+
+}
+function quit(){
+    console.log("Goodbye")
+    process.exit()
+
 }
 mainmenu()
